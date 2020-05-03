e698b022c575b96ee0ccbfbcd39dd0205fbb1bc9
Gabriel Mendez <gabrielmendez.dev@gmail.com>
2020-05-04 01:46:18 +0200

    add test cookie

--- packages/logging-service/src/resolvers/Log.ts
+++ packages/logging-service/src/resolvers/Log.ts
@@ -1,11 +1,14 @@
-import { Resolver, Query } from 'type-graphql';
+import { Resolver, Query, Ctx } from 'type-graphql';
 import { logs } from '../data';
 import Log from '../entities/Log';
+import { Request, Response } from 'express';
 
 @Resolver(() => Log)
 export default class LogsResolver {
   @Query(() => Log)
-  testLogs(): Log {
+  testLogs(@Ctx() { req, res }: { req: Request; res: Response }): Log {
+    console.log(req);
+    res.cookie('lol', 'lmao');
     return logs[0];
   }
 }
