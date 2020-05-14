c7214ea2107a93be9585d060f5ce115d0478d2dc
Gabriel Mendez <gabrielmendez.dev@gmail.com>
2020-05-15 00:18:55 +0200

    provide better typing for gateway remote source

--- packages/api-gateway/src/datasources/GatewayDataSource.ts
+++ packages/api-gateway/src/datasources/GatewayDataSource.ts
@@ -1,14 +1,24 @@
 import { RemoteGraphQLDataSource } from '@apollo/gateway';
 import parseCookies from '../helpers/parseCookies';
+import { GatewayContext } from '../typings';
+
+type WillSendRequestParams = {
+  request: any;
+  context: GatewayContext;
+};
 
 export default class GatewayDataSource extends RemoteGraphQLDataSource {
-  willSendRequest({ request, context }): void {
+  willSendRequest({ request, context }: WillSendRequestParams): void {
     if (context && context.req && context.req.cookies) {
+      // Perform authentication here
+      console.log(context.req.cookies);
+
       request.http.headers.set('Cookie', JSON.stringify(context.req.cookies));
     }
   }
 
-  didReceiveResponse({ response, context }): typeof response {
+  // The context here is also the gateway's
+  didReceiveResponse({ response, context }: any): typeof response {
     const rawCookies = response.http.headers.get('set-cookie') as string | null;
 
     if (rawCookies) {
