7ceaeb8ef6de89e4d8004f2d9826f4fac3109bb7
Gabriel Mendez <gabrielmendez.dev@gmail.com>
2020-05-25 18:56:20 +0200

    add favicon and logo to client

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import Logo from './assets/logo.svg';
 
 function App() {
   return (
     <div className='App'>
       <header className='App-header'>
+        <img src={Logo} alt='dsaf' />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
