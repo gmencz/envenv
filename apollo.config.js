a9fea1319c675c33a6adc33bfe3687fde0737630
Gabriel Mendez <gabrielmendez.dev@gmail.com>
2020-05-26 15:19:43 +0200

    setup apollo config with graphql-codegen without clashing

--- apollo.config.js
+++ apollo.config.js
@@ -2,5 +2,6 @@ module.exports = {
   client: {
     service: `envenv-main-graph@development`, // "development" is the graph variant we're using
     includes: ['./packages/client/src/**/*.{ts,tsx,js,jsx,graphql}'],
+    excludes: ['./packages/client/src/generated/*.{tsx, ts}'], // exclude the generated types to avoid clashing
   },
 };
--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,18 +1,13 @@
 import React from 'react';
 import { FullpageLoader } from './components/fullpage-loader';
 import { useQuery } from '@apollo/client';
-import { WhoAmI } from './generated/WhoAmI';
 import { ME } from './graphql/user/me';
 
 const AuthenticatedApp = React.lazy(() => import('./authenticated-app'));
 const UnauthenticatedApp = React.lazy(() => import('./unauthenticated-app'));
 
 const App: React.FC = () => {
-  const { data, loading } = useQuery<WhoAmI>(ME);
-
-  if (loading) {
-    return <FullpageLoader />;
-  }
+  const { data } = useQuery(ME);
 
   return (
     <React.Suspense fallback={<FullpageLoader />}>
