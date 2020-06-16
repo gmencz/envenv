abad9455ceb6e33d642a29107d8e50d0167859bd
Gabriel Méndez <gabrielmendez.dev@gmail.com>
2020-06-17 01:07:40 +0200

    :construction_worker: attempt to fix job #9

--- packages/accounts-service/apollo.config.js
+++ packages/accounts-service/apollo.config.js
@@ -1,15 +1,9 @@
 require('dotenv').config();
-/*
-  Whenever we want to push our service to AGM, we have to do it in the running container
-  because we're making use of its environment variables, so bash into the container and run
-  "npm run apollo-push:<environment>", environment being "dev" or "prod" or "test" or some other
-  environment.
-*/
 module.exports = {
   service: {
     name: 'envenv-main-graph',
     endpoint: {
-      url: process.env.GRAPHQL_ENDPOINT,
+      url: 'http://localhost:5000/graphql',
     },
   },
 };
